93695dd561af2bb790e487bf48c29934bffba7f6
Cauan Cem <cacocembritosoares@gmail.com>
2024-08-28 16:47:42 -0300

    arquivos spring

---
+++ spring-verzel-plus/src/main/java/spring/verzel_plus/VerzelPlusApplication.java
@@ -0,0 +1,13 @@
+package spring.verzel_plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerzelPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerzelPlusApplication.class, args);
+	}
+
+}
