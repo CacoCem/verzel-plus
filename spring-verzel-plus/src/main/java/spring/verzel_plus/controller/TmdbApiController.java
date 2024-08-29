fd4e81794e3be1e5050b8b2a949edd04730bb762
Cauan Cem Brito Soares <85462017+CacoCem@users.noreply.github.com>
2024-08-29 13:06:03 -0300

    dev envr

--- spring-verzel-plus/src/main/java/spring/verzel_plus/controller/TmdbApiController.java
+++ spring-verzel-plus/src/main/java/spring/verzel_plus/controller/TmdbApiController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin(origins = "https://angular-verzel-plus-gw5m.vercel.app")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class TmdbApiController {
 
